707165259044a965062b0adc995dfe87d78d629e
mrmiruo02 <mrmiruo@gmail.com>
2025-03-21 14:11:31 +0800

    add default error handler in globalErrorHandler

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -5,6 +5,9 @@ const errorHandler = (err: Error, req: Request, res: Response, next: NextFunctio
   if (err instanceof CustomError) {
     res.status(err.errorCode).send({ errors: err.serializeErrors() })
   }
+
+  // Default error response for unexpected errors
+  res.status(500).json({ message: "Internal Server Error" });
 };
 
 export default errorHandler;
