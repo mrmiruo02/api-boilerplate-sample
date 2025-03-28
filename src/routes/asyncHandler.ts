8129d346c6b41b3b1623210d56c18f38aa7d1ac0
mrmiruo02 <mrmiruo@gmail.com>
2025-03-28 16:07:53 +0800

    update rules add no-unused-vars

--- src/routes/asyncHandler.ts
+++ src/routes/asyncHandler.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 // Wrapper function for async error handling
 const asyncHandler =
-  (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) =>
+  (fn: (_req: Request, _res: Response, _next: NextFunction) => Promise<void>) =>
   (req: Request, res: Response, next: NextFunction) => {
     fn(req, res, next).catch(next); // Automatically catches errors and forwards them
   };
--- types/exif-parser.d.ts
+++ types/exif-parser.d.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 declare module 'exif-parser' {
+  // eslint-disable-next-line no-unused-vars
   interface ExifData {
     tags: Record<string, any>;
     imageSize: { width: number; height: number };
   }
 
   class ExifParser {
-    static create(buffer: Buffer): ExifParser;
+    static create(_buffer: Buffer): ExifParser;
     parse(): { tags: Record<string, any> };
   }
 
